b61229eb2c5119bc993fea70d6b50450b7fd8c96
rotieno <rabinyitzahck@gmail.com>
2024-08-26 20:45:05 +0300

    chore: add functions

---
+++ more-or-less.js
@@ -0,0 +1,13 @@
+const more = (n) => n + 1;
+const resultMore = more(4);
+
+const less = (n) => n - 1;
+const resultLess = less(4);
+
+const add = (m,n) => m + n;
+const resultAdd = add(4,2);
+
+const sub = (m,n) => m - n;
+const resultSub = sub(4,2)
+
+
