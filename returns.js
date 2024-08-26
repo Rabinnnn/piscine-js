54af5e3af53a5a1327c1bdf7910e2704f7bc1520
rotieno <rabinyitzahck@gmail.com>
2024-08-26 20:56:05 +0300

    chore: add returns

--- returns.js
+++ returns.js
@@ -1,6 +1,14 @@
-const id = (n) => n
+/*const id = (n) => n
 const resultId = id(n)
 
 const getLength = (arr) => arr.length
 const arr = [2, 3]
-const resultLength = getLength(arr)
+const resultLength = getLength(arr) */
+
+function id(input) {
+    return input
+}
+
+function getLength(input) {
+    return input.length
+}
