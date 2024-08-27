b2bbf099ae2a9d367f17ed5451655fce689ffbe8
rotieno <rabinyitzahck@gmail.com>
2024-08-27 12:27:59 +0300

    fix: ensure the value does not exceed the max safe value

--- biggie-smalls.js
+++ biggie-smalls.js
@@ -1,5 +1,10 @@
 // const smalls = Number.MIN_SAFE_INTEGER
 // const biggie = Number.MAX_SAFE_INTEGER
 
-const smalls = -(2 ** 63);
-const biggie = 2 ** 63 - 1;
+const smalls = Number.MIN_VALUE; 
+const biggie = Number.MAX_VALUE;
+
+// const smalls = -(2 ** 63);
+// const biggie = 2 ** 63 - 1;
+
+//console.log(biggie > 1.7976931348623157e308);
