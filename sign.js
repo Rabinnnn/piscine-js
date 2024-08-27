7c4c278e1aa517d7b933340615eff65a08703064
rotieno <rabinyitzahck@gmail.com>
2024-08-27 18:03:27 +0300

    chore: add function determining sign of numbers

---
+++ sign.js
@@ -0,0 +1,16 @@
+function sign(n){
+    if (n > 0){
+        return 1
+    }else if (n < 0){
+        return -1
+    }else if (n === 0){
+        return 0
+    }
+}
+
+function sameSign(a, b){
+    if (sign(a) === sign(b)){
+        return true
+    }
+    return false
+}
