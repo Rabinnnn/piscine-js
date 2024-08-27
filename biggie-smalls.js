7e05c5002199ec1fb093cd805a8e4ba2636505b3
rotieno <rabinyitzahck@gmail.com>
2024-08-27 12:44:59 +0300

    biggie-smalls

--- biggie-smalls.js
+++ biggie-smalls.js
@@ -1,10 +1,3 @@
-// const smalls = Number.MIN_SAFE_INTEGER
-// const biggie = Number.MAX_SAFE_INTEGER
 
 const smalls = Number.MIN_VALUE; 
 const biggie = Number.MAX_VALUE;
-
-// const smalls = -(2 ** 63);
-// const biggie = 2 ** 63 - 1;
-
-//console.log(biggie > 1.7976931348623157e308);
