4e418825ac02001ded14350be86978a8485340ee
rotieno <rabinyitzahck@gmail.com>
2024-08-28 19:49:01 +0300

    chore: create function that add block to blockchain

---
+++ block-chain.js
@@ -0,0 +1,14 @@
+const hashCode = str =>  (    [...str].reduce((h, c) => (h = (h << 5) - h + c.charCodeAt(0)) & h, 0) >>> 0  ).toString(36)
+
+function blockChain(data, prev = { index: 0, hash: '0'   
+ }) {
+  const index = prev.index + 1;
+  const hash = hashCode(`${index}${prev.hash}${JSON.stringify(data)}`);
+  return {
+    index,
+    hash,
+    data,
+    prev,
+    chain: newData => blockChain(newData, this),
+  };
+}
