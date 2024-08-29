f8c31f2c1a14eebe9e0aeabd9b33591adfb7bcc4
rotieno <rabinyitzahck@gmail.com>
2024-08-29 17:24:45 +0300

    feat: implement 2 functions that work like String.split and Array.join

--- unbreakable.js
+++ unbreakable.js
@@ -1,33 +1,35 @@
-function split(str, separator) {
-    let result = [];
-    let startIndex = 0;
-    
-    while (true) {
-      let index = str.indexOf(separator, startIndex);
-      
-      if (index === -1) {
-        result.push(str.slice(startIndex));
-        break;
-      }
-      
-      result.push(str.slice(startIndex, index));
-      startIndex = index + separator.length;
+function split(str, sep) {
+   
+    if (sep === null) {
+        sep = ",";
     }
-    
+    var result = [];
+    if (sep === "") {
+        for (var i = 0; i < str.length; i++) {
+            result.push(str[i]);
+        }
+        return result;
+    }
+    var end = str.indexOf(sep);
+    while (end > -1) {
+        end = str.indexOf(sep);
+        if (end === -1) {
+            break;
+        }
+        result.push(str.slice(0, end));
+        str = str.slice(end + sep.length);
+    }
+    result.push(str);
     return result;
-  }
+}
 
-  function join(array, separator) {
-    let result = '';
-    
-    for (let i = 0; i < array.length; i++) {
-      result += array[i];
-      
-      if (i < array.length - 1) {
-        result += separator;
-      }
+function join(arr, sep) {
+    if (sep === null) {
+        sep = ",";
     }
-    
+    var result = arr[0].toString();
+    for (var i = 1; i < arr.length; i++) {
+        result += sep + arr[i];
+    }
     return result;
-  }
-  
+}
