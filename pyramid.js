dc7c09bcde13db4bf12661b9868c84eeec17a082
rotieno <rabinyitzahck@gmail.com>
2024-08-30 20:59:40 +0300

    feat: add a function that creates a pyramid

--- pyramid.js
+++ pyramid.js
@@ -2,7 +2,7 @@ function pyramid(char, depth) {
     let output = '';
 
     for (let i = 1; i <= depth; i++) {
-        const spaces = ' '.repeat(depth - i); 
+        const spaces = ' '.repeat(char.length).repeat(depth - i); 
         const characters = char.repeat(2 * i - 1);   
         output += spaces + characters + '\n';        
     }
