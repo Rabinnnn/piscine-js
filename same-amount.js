aa48af59d64479340e2584e6dadd11edf35b190b
rotieno <rabinyitzahck@gmail.com>
2024-08-31 13:13:58 +0300

    feat: add function that takes a string and 2 regular expressions then confirms that the expressions match the string the same number of times

--- same-amount.js
+++ same-amount.js
@@ -2,7 +2,7 @@ function sameAmount(str, reg1, reg2){
     r1 = new RegExp(reg1, "g")
     r2 = new RegExp(reg2, "g")
 
-    const match1 = str.match(r1)
-    const match2 = str.match(r2)
+    let match1 = str.match(r1)
+    let match2 = str.match(r2)
 return match1 !== null && match2 !== null && match1.length === match2.length
 }
