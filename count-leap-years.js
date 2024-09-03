36bb57948bf4290097007afdef6dcc2c0b72f6ce
rotieno <rabinyitzahck@gmail.com>
2024-09-03 17:15:40 +0300

    feat: add functions that counts the number of leap years since year 1

---
+++ count-leap-years.js
@@ -0,0 +1,9 @@
+function countLeapYears(date) {
+    let count = 0;
+    for (let i = 1; i < date.getFullYear(); i++) {
+        if (i % 4 === 0 && (i % 100 !== 0 || i % 400 === 0)) {
+            count++;
+        }
+    }
+    return count;
+}
