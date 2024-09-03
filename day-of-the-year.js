456f362d807d97333493b4d37e058f6c68872ff8
rotieno <rabinyitzahck@gmail.com>
2024-09-03 17:22:40 +0300

    feat: add function that returns number of days since the first day of the year given in a date

---
+++ day-of-the-year.js
@@ -0,0 +1,11 @@
+function dayOfTheYear(date) {
+    
+    const year = date.getFullYear();
+  
+    const startOfYear = new Date(year, 0, 1);
+  
+    const difference = date - startOfYear;
+    const output = Math.floor(difference / (1000 * 60 * 60 * 24));
+  
+    return output + 1;
+  }
