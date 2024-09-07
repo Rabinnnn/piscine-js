081c6a120631cbbfcef8da197e084db2b01fa625
rotieno <rabinyitzahck@gmail.com>
2024-09-07 07:57:27 +0300

    feat: add solution to pimp-my-style.js

---
+++ pimp-my-style.js
@@ -0,0 +1,21 @@
+import { styles } from "./pimp-my-style.data.js";
+var count = 0;
+
+function pimp() {
+    var button = document.querySelector("button.button");
+    if (!button.classList.contains("unpimp")) {
+        button.classList.add(styles[count]);
+        count++;
+    } else {
+        count--;
+        button.classList.remove(styles[count]);
+        if (count === 0) {
+            button.classList.toggle("unpimp");
+        }
+    }
+    if (count === styles.length) {
+        button.classList.toggle("unpimp");
+    }
+}
+
+export { pimp };
