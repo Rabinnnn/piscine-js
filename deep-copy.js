f13779bde8be62bda25969c2b64d4cedd231cfa0
rotieno <rabinyitzahck@gmail.com>
2024-09-11 11:21:29 +0300

    feat: add function that copies objects and arrays recursively

--- deep-copy.js
+++ deep-copy.js
@@ -1,17 +1,27 @@
-function deepCopy(input){
-  if (Array.isArray(input)){
-    var result = []
-    for(var i = 0; i < input.length; i++){
-        result[i] = deepCopy(input[i])
+function deepCopy(input) {
+    if (Array.isArray(input)) {
+        var result = [];
+        for (var i = 0; i < input.length; i++) {
+            result[i] = deepCopy(input[i]);
+        }
+        return result;
+    } else if (isObject(input)) {
+        var result = {};
+        for (var key in input) {
+            result[key] = deepCopy(input[key]);
+        }
+        return result;
+    } else {
+        return input;
     }
-    return result
-  } else if ((typeof input === "object") && !(Array.isArray(input) && !(input === null) && !(input instanceof RegExp)) ){
-    var result = {}
-    for (var key in input){
-        result[key] = deepCopy(input[key])
-    }
-    return result
-  }else{
-    return input
-  }
+}
+
+function isObject(input) {
+    return (
+        typeof input === "object" &&
+        !(typeof input === "function") &&
+        !Array.isArray(input) &&
+        input !== null &&
+        !(input instanceof RegExp)
+    );
 }
