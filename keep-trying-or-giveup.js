6dd847b5798b3ad73f76c998dfb6bd9ea1f1f95c
rotieno <rabinyitzahck@gmail.com>
2024-09-13 20:00:00 +0300

    feat: add keep-trying-or-giveup.js

--- keep-trying-or-giveup.js
+++ keep-trying-or-giveup.js
@@ -1,32 +1,21 @@
-function retry(count = 3, callback = async () => {}) {
-    return async function (...args) {
-        try {
-            const res = await callback(...args);
-            return res;
-        } catch (e) {
-            if (count > 0) {
-                return retry(count - 1, callback)(...args);
-            } else {
-                throw e;
+function retry(count, func) {
+    return async (...args) => {
+        while (true) {
+            try {
+                return await func(...args)
+            } catch (e) {
+                if (count === 0) throw e
+                count--
             }
         }
-    };
+    }
 }
 
-function timeout(delay = 0, callback = async () => {}) {
-    return async function (...args) {
-        const timeout = new Promise((resolve) =>
-            setTimeout(resolve, delay, Error('timeout'))
-        );
-        const functionCall = new Promise((resolve) =>
-            resolve(callback(...args))
-        );
-        const res = await Promise.race([timeout, functionCall]).then(
-            (res) => res
-        );
-        if (res instanceof Error) {
-            throw res;
-        }
-        return res;
-    };
+function timeout(delay, func) {
+    return async (...args) => {
+        let start = new Date()
+        const result = await func(...args)
+        if (new Date() - start <= delay) return result
+        throw new Error('timeout')
+    }
 }
